b8d58123761f75d126a8ab1ce713ca51118dcb01
Sumit-ParshadGit <sumit.sp776@gmail.com>
2026-08-04 23:36:15 +0530

    Time: 112 ms (5.09%), Space: 81.7 MB (9.06%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp[501][501];
+    int sol(string w1,string w2,int i,int j,int n1,int n2){
+        if(j>=n2)return n1-i;
+        if(i>=n1)return n2-j;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(w1[i]==w2[j])return dp[i][j] =  sol(w1,w2,i+1,j+1,n1,n2);
+        return dp[i][j] = 1+ min(
+            {sol(w1,w2,i+1,j,n1,n2),
+            sol(w1,w2,i+1,j+1,n1,n2),
+            sol(w1,w2,i,j+1,n1,n2)}
+        );
+    }
+    int minDistance(string word1, string word2) {
+        int n1 = word1.size();
+        int n2 = word2.size();
+        memset(dp,-1,sizeof(dp));
+        return sol(word1,word2,0,0,n1,n2);
+    }
+};
