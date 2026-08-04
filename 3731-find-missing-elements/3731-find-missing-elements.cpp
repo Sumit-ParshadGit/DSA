c6b6c3fcd867b01dc739d0de22bb640670b0c0aa
Sumit-ParshadGit <sumit.sp776@gmail.com>
2026-08-04 21:56:30 +0530

    Time: 0 ms (100%), Space: 32.1 MB (91.09%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,17 +1,29 @@
+// class Solution {
+// public:
+//     vector<int> findMissingElements(vector<int>& nums) {
+//         map<int,int>mp;
+//         int ma = 0;
+//         int mi = INT_MAX;
+//         for(auto&el:nums){
+//             mp[el]=1;   
+//             ma = max(ma,el);
+//             mi = min(mi,el);
+//         }
+//         vector<int>ans;
+//         for(int i = mi;i<ma;i++){
+//             if(!mp[i])ans.push_back(i);
+//         }return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        map<int,int>mp;
-        int ma = 0;
-        int mi = INT_MAX;
-        for(auto&el:nums){
-            mp[el]=1;   
-            ma = max(ma,el);
-            mi = min(mi,el);
-        }
+        sort(nums.begin(),nums.end());
         vector<int>ans;
-        for(int i = mi;i<ma;i++){
-            if(!mp[i])ans.push_back(i);
+        for(int i = 0;i<nums.size()-1;i++){
+            for(int j = nums[i]+1;j<nums[i+1];j++){
+                ans.push_back(j);
+            }
         }return ans;
     }
 };
